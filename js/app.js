1b269b78ca3ee3301466f159b77b51b578f47770
Martin Delille <martin@phonations.com>
2015-10-27 22:14:50 +0100

    display next week day

--- js/app.js
+++ js/app.js
@@ -3,11 +3,24 @@ var celciusFromKelvin = function(kelvin) {
 };
 
 $(function() {
-  var token = 'eb2d3efe1c95bd690a3e7339b42e387c';
+  var weekday = new Array(7);
+  weekday[0]=  "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var today = new Date().getDay();
+  for(var i = 1; i < 4; i++) {
+    $('#week .col-md-4:nth-child(' + i + ') h3').text(weekday[(today + i) % 7]);
+  }
 
   $.getJSON('http://ipinfo.io/', function(data) {
     $('#city').text(data.city);
 
+    var token = 'eb2d3efe1c95bd690a3e7339b42e387c';
     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + data.city + ',' + data.country + '&appid=' + token;
     $.getJSON(url, function(data) {
       console.log(data);
